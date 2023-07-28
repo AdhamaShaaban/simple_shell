caeee610adbecbe3083bd0485bc32d6cbf69cb15
AdhamaShaaban <poet_adham@yahoo.com>
2023-07-28 16:20:18 -0700

    Simple_Shell

---
+++ exits.c
@@ -0,0 +1,74 @@
+#include "shell.h"
+
+/**
+ **_strncpy - String copies
+ *@dest: String copied to destination
+ *@src: String source
+ *@n: num of copied characters
+ *Return: String concatenat
+ */
+char *_strncpy(char *dest, char *src, int n)
+{
+	int i, j;
+	char *s = dest;
+
+	i = 0;
+	while (src[i] != '\0' && i < n - 1)
+	{
+		dest[i] = src[i];
+		i++;
+	}
+	if (i < n)
+	{
+		j = i;
+		while (j < n)
+		{
+			dest[j] = '\0';
+			j++;
+		}
+	}
+	return (s);
+}
+
+/**
+ **_strncat - dual string concat
+ *@dest: 1st string
+ *@src: 2nd string
+ *@n: Num of bytes to be used in max
+ *Return: String concatenat
+ */
+char *_strncat(char *dest, char *src, int n)
+{
+	int i, j;
+	char *s = dest;
+
+	i = 0;
+	j = 0;
+	while (dest[i] != '\0')
+		i++;
+	while (src[j] != '\0' && j < n)
+	{
+		dest[i] = src[j];
+		i++;
+		j++;
+	}
+	if (j < n)
+		dest[i] = '\0';
+	return (s);
+}
+
+/**
+ **_strchr - find a string charact
+ *@s: parse the string
+ *@c: char looking for
+ *Return: (s) pointer to area of the memory s
+ */
+char *_strchr(char *s, char c)
+{
+	do {
+		if (*s == c)
+			return (s);
+	} while (*s++ != '\0');
+
+	return (NULL);
+}
